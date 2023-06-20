e3576d9db0c287ef3801a3accd384057e5d1fbc9
Jedicito <seron.cesar@gmail.com>
2023-06-19 20:44:40 -0400

    Agregando Toast

--- app/src/main/java/chl/ancud/m4_individual5/MainActivity.java
+++ app/src/main/java/chl/ancud/m4_individual5/MainActivity.java
@@ -3,6 +3,10 @@ package chl.ancud.m4_individual5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initListeners();
+    }
+
+    private void initListeners() {
+        Button btn_showToast = findViewById(R.id.btn_showToast);
+        Button btn_addCounter = findViewById(R.id.btn_addCounter);
+        TextView txV_contador = findViewById(R.id.txV_contador);
+
+        btn_showToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Hola", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
