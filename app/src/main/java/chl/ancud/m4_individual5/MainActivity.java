5ef22e016194be69eba42a2db7e6000ec2d66ce8
Jedicito <seron.cesar@gmail.com>
2023-06-19 20:33:57 -0400

    Commit Inicial

---
+++ app/src/main/java/chl/ancud/m4_individual5/MainActivity.java
@@ -0,0 +1,14 @@
+package chl.ancud.m4_individual5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
